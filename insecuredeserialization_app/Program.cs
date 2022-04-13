49bef4b3b27b94266cc451aed8ad29baa6d52104
marcelo <marcelo@marcelo>
2022-04-13 20:08:25 -0300

    Insecure deserialization app

--- insecuredeserialization_app/Program.cs
+++ insecuredeserialization_app/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Web.Script.Serialization;
 
 public class Book
 {
@@ -30,11 +32,28 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		// Book book1 = new Book('Lord of The Rings', 'Fantasy');
+		// Setting console to accept more than 254 char
+		Console.SetIn(new StreamReader(Console.OpenStandardInput(), Console.InputEncoding, false, bufferSize: 1024));
+
+		// Instantiating serializer in insecure mode
+		JavaScriptSerializer serializer = new JavaScriptSerializer(new SimpleTypeResolver());
+
+		// Initializing book object and serializing it
 		Book book1 = new Book();
-		book1.Name = 'Lord of The Rings';
-		book1.Category = 'Fantasy';
-		Console.WriteLine(book1.Name);
-		Console.WriteLine(book1.Category);
+		book1.Name = "Lord of The Rings";
+		book1.Category = "Fantasy";
+
+		var jsonBook = serializer.Serialize(book1);
+
+		// Print Book JSON
+		Console.WriteLine("Json content: \n" + jsonBook);
+
+		// Read new JSON and deserialize it into Book object
+		Console.WriteLine("\nInsert modified JSON: ");
+		string editedBookJson = Console.ReadLine();
+
+		Book lotrBook = serializer.Deserialize<Book>(editedBookJson);
+		Console.WriteLine("Book object name (post JSON deserialization): " + lotrBook.Name);
+		Console.WriteLine("Book object category (post JSON deserialization): " + lotrBook.Category);
 	}
 }
