dae0a4ec84819c087933b7e383333ca1d7c86c78
marcelo <marcelo@marcelo>
2022-04-13 19:55:47 -0300

    Add project files.

---
+++ insecuredeserialization_app/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Book
+{
+	private string _name;
+	private string _category;
+
+	public string Name
+	{
+		get { return _name; }
+		set
+		{
+			_name = value;
+		}
+	}
+
+	public string Category
+	{
+		get { return _category; }
+		set
+		{
+			_category = value;
+		}
+	}
+}
+
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Book book1 = new Book('Lord of The Rings', 'Fantasy');
+		Book book1 = new Book();
+		book1.Name = 'Lord of The Rings';
+		book1.Category = 'Fantasy';
+		Console.WriteLine(book1.Name);
+		Console.WriteLine(book1.Category);
+	}
+}
